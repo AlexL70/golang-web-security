765f708fde92e96486474b0e6b90a6d212a28aa6
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-05-09 19:16:06 +0300

    16. Hashing passwords

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/AlexL70/golang-web-security
 
 go 1.18
+
+require golang.org/x/crypto v0.0.0-20220507011949-2cf3adece122 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	//	Base64 encoding is used to encode username and password in "Authorization" HTTP
-	//	header when using basic HTTP authentication
-	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
+	pass := "123456789"
+	hashedPass, err := hashPassword(pass)
+	if err != nil {
+		panic(err)
+	}
+	//pass = "12345678"
+	err = comparePassword(pass, hashedPass)
+	if err != nil {
+		log.Fatalln("Not logged in.", err)
+	}
+	log.Println("Logged in!")
+}
+
+func hashPassword(password string) ([]byte, error) {
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("Error encrypting password: %w", err)
+	}
+	return bs, nil
+}
+
+func comparePassword(passowrd string, hashedPass []byte) error {
+	err := bcrypt.CompareHashAndPassword(hashedPass, []byte(passowrd))
+	if err != nil {
+		return fmt.Errorf("Invalid password: %w", err)
+	}
+	return nil
 }
