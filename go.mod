b95cecfaf84299d3070dce8063f47503f86929a7
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-05-22 19:43:48 +0300

    29. Encrypting an email

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.18
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	golang.org/x/crypto v0.0.0-20220507011949-2cf3adece122 // indirect
+	golang.org/x/crypto v0.0.0-20220518034528-6f7dac969898 // indirect
 )
--- main.go
+++ main.go
@@ -1,27 +1,66 @@
 package main
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"encoding/base64"
 	"fmt"
 	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
 	msg := "This is totally fun get hands-on and learning it from the ground up. Thank you for sharing this info with me and helping me."
-	encoded := encode(msg)
+	password := "My new secure password"
+	key, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	logErrorIfAny(err)
+	key = key[:16]
+	encrypted, err := cipherMsg(key, []byte(msg))
+	logErrorIfAny(err)
+	encoded := encode(encrypted)
 	fmt.Println(encoded)
-	decoded := decode(encoded)
-	fmt.Println(decoded)
+	decoded, err := decode(encoded)
+	logErrorIfAny(err)
+	decrypted, err := cipherMsg(key, decoded)
+	logErrorIfAny(err)
+	fmt.Println(string(decrypted))
 }
 
-func encode(msg string) string {
-	return base64.URLEncoding.EncodeToString([]byte(msg))
+func logErrorIfAny(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
-func decode(encoded string) string {
+func cipherMsg(key []byte, msg []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("Error getting new cipher")
+	}
+	s := cipher.NewCTR(block, make([]byte, aes.BlockSize) /*initialization vector*/)
+	buff := &bytes.Buffer{}
+	sw := cipher.StreamWriter{
+		S: s,
+		W: buff,
+	}
+	defer sw.Close()
+	_, err = sw.Write([]byte(msg))
+	if err != nil {
+		return nil, fmt.Errorf("Error writing message to buffer: %w", err)
+	}
+	return buff.Bytes(), nil
+}
+
+func encode(msg []byte) string {
+	return base64.URLEncoding.EncodeToString(msg)
+}
+
+func decode(encoded string) ([]byte, error) {
 	result, err := base64.URLEncoding.DecodeString(encoded)
 	if err != nil {
-		log.Panicln(err)
+		return nil, fmt.Errorf("Error decoding base64 string: %w", err)
 	}
-	return string(result)
+	return result, nil
 }
